23b0ac669c7b878a8e94b27845e2c688f43302bf
Daniel Westerling <danwe432@student.liu.se>
2018-11-29 23:26:20 +0100

    camera fixes
    rcamera

--- spel/Assets/Scripts/InputManager.cs
+++ spel/Assets/Scripts/InputManager.cs
@@ -5,28 +5,48 @@ using UnityEngine;
 public class InputManager : MonoBehaviour {
 
     public float panSpeed;
+    public float rotateSpeed;
+    public float rotateAmount;
+    
+    private Quaternion rotation;
 
+    private float panDetect = 15f;
+    private float minHeight = 10f;
+    private float maxHeight = 100f;
     private float m_panDetect = 15f;
+
     private float moveX;
     private float moveZ;
+    private float moveY;
     private float xPos;
     private float yPos;
+
     private Vector3 m_newPos;
+    
 
 	void Start () {
 
         m_newPos = new Vector3(0, 0, 0);
+
+        rotation = Camera.main.transform.rotation;
     }
 	
 	void Update () {
 
         MoveCamera();
+        RotateCamera();
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Camera.main.transform.rotation = rotation;
+        }
 	}
 
     void MoveCamera()
     {
         moveX = Camera.main.transform.position.x;
         moveZ = Camera.main.transform.position.z;
+        moveY = Camera.main.transform.position.y;
 
         xPos = Input.mousePosition.x;
         yPos = Input.mousePosition.y;
@@ -54,8 +74,27 @@ public class InputManager : MonoBehaviour {
             moveZ -= panSpeed;
         }
 
-        m_newPos = new Vector3(moveX, 0, moveZ);
+        moveY -= Input.GetAxis("Mouse ScrollWheel") * (panSpeed *20);
+        moveY = Mathf.Clamp(moveY, minHeight, maxHeight);
+
+        m_newPos = new Vector3(moveX, moveY, moveZ);
 
         Camera.main.transform.position = m_newPos;
+    }
+
+    void RotateCamera()
+    {
+        Vector3 origin = Camera.main.transform.eulerAngles;
+        Vector3 destination = origin;
+
+        if(Input.GetMouseButton(2))
+        {
+            destination.x -= Input.GetAxis("Mouse Y") * rotateAmount;
+            destination.y += Input.GetAxis("Mouse X") * rotateAmount;
+        }
+        if(destination != origin)
+        {
+            Camera.main.transform.eulerAngles = Vector3.MoveTowards(origin, destination, Time.deltaTime * rotateSpeed);
+        }
     }
 }
